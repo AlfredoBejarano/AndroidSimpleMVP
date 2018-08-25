4fc7252445e55288e060761c6ab9411aff97b74b
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-08-25 14:27:04 -0500

    Abstract more functionallity from SimplePResenter

--- simplemvp/src/main/java/com/alfredobejarano/simplemvp/presenter/SimplePresenter.kt
+++ simplemvp/src/main/java/com/alfredobejarano/simplemvp/presenter/SimplePresenter.kt
@@ -1,7 +1,7 @@
 package com.alfredobejarano.simplemvp.presenter
 
+import android.arch.lifecycle.MutableLiveData
 import android.os.Handler
-import android.util.Log
 import com.alfredobejarano.simplemvp.view.SimpleView
 
 /**
@@ -13,50 +13,47 @@ import com.alfredobejarano.simplemvp.view.SimpleView
  */
 abstract class SimplePresenter(protected var view: SimpleView?) {
     /**
-     * Sends a message to the attached view.
-     * * If the view Context is not null, the message is sent to it.
-     * * If the view is null (let's say, while Unit Testing), the message is printed to the LogCat.
-     *
-     * @param message The message object to be sent to the view.
+     * Status value that reports if this presenter is busy or not.
+     * Wrapped in [MutableLiveData] allowing a [LifecycleOwner][android.arch.lifecycle.LifecycleOwner]
+     * to observe it.
      */
-    fun sendMessage(message: Any) {
-        // Check if the View's Context is not null.
-        view?.asContext()?.let {
-            Handler(it.mainLooper).post {
-                // Send the display message if it is not null.
-                view?.displayMessage(message)
-            }
-        } ?: run {
-            // If the view Context is null, print the message String value.
-            Log.d(this::class.java.simpleName, message.toString())
+    val status = MutableLiveData<Status>()
+
+    /**
+     * This function will remove the reference to the given view,
+     * preventing leakages of Context child classes as Views usually
+     * extends from the Context class.
+     */
+    fun destroyView() {
+        if (view != null) {
+            view = null
         }
     }
 
     /**
-     * Reports to the view that this presenter is doing some processing work.
-     * If the view is null (let's say, while Unit testing) the isWorking value
-     * gets printed in the LogCat.
-     *
-     * @param isWorking If the presenter is working or not.
+     * Sends a message object to the view.
      */
-    fun reportProgress(isWorking: Boolean) {
-        // Check if the View's Context is not null.
-        view?.asContext()?.let {
-            Handler(it.mainLooper).post {
-                // Report the presenter processing status to the view.
-                view?.displayLoadingView(isWorking)
-            }
-        } ?: run {
-            // If the view Context is null, print the Status to the LogCat.
-            Log.d(this::class.java.simpleName, "Presenter is working: $isWorking")
-        }
+    protected fun sendMessage(payload: Any?) = runOnUIThread {
+        view?.displayMessage(payload)
     }
 
     /**
-     * Destroy the view reference for this
-     * presenter for it to be garbage collected.
+     * Executes a function in the UI thread.
      */
-    fun onDestroyView() {
-        view = null
+    private fun runOnUIThread(f: () -> Unit) =
+            Handler(view?.asContext()?.mainLooper).post(f)
+
+    /**
+     * Enum class that defines the current mStatus of a presenter.
+     */
+    enum class Status {
+        /**
+         * This value defines that the presenter is doing work.
+         */
+        STATUS_BUSY,
+        /**
+         * This value defines that the presenter is done doing work.
+         */
+        STATUS_DONE
     }
 }
